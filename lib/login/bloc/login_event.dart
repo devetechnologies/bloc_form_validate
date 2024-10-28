3dcb37db4f9ba99a043693dea5b4f219d24fa8ee
devetechnologies <devetechnologies@gmail.com>
2024-10-28 12:50:52 -0400

    create loginEvent bloc

--- lib/login/bloc/login_event.dart
+++ lib/login/bloc/login_event.dart
@@ -6,3 +6,25 @@ sealed class LoginEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+final class LoginUsernameChanged extends LoginEvent {
+  const LoginUsernameChanged(this.username);
+
+  final String username;
+
+  @override
+  List<Object> get props => [username];
+}
+
+final class LoginPasswordChanged extends LoginEvent {
+  const LoginPasswordChanged(this.password);
+
+  final String password;
+
+  @override
+  List<Object> get props => [password];
+}
+
+final class LoginSubmitted extends LoginEvent {
+  const LoginSubmitted();
+}
